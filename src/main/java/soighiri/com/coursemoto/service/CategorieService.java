742ef969b5965c1798f2754360bf27f7aa88ca25
SaidSoighiri94 <msaidsoighir@gmail.com>
2023-07-28 00:13:37 +0200

    gestion de course moto

--- src/main/java/soighiri/com/coursemoto/service/CategorieService.java
+++ src/main/java/soighiri/com/coursemoto/service/CategorieService.java
@@ -1,4 +1,28 @@
 package soighiri.com.coursemoto.service;
 
+import soighiri.com.coursemoto.dto.CategorieDto;
+import soighiri.com.coursemoto.model.Categorie;
+
+import java.util.List;
+
 public interface CategorieService {
+
+    //Sauvegarder une categorie unecategorie en utlisant un Dto
+    Categorie  saveCategorieFromCategorieDto(CategorieDto categorieDto);
+
+    //Mettre a jour une categorie en utilisant une categorieDto
+    Categorie updateCategorieFromCategorieDto(CategorieDto categorieDto);
+
+    //Supprimer une categorie de la base de donnée en fonction de l'Id
+
+    void deleteCategorie(Long idCategorie);
+
+    //Récupère un circuit en fonction de son ID
+    Categorie getCategorieById(Long idCategorie);
+
+    //afficher la liste de tous les categorie
+   List<Categorie> getAllCategories();
+
+   // Covertir un entity vers un Dto
+    CategorieDto convertEntityToDto(Categorie categorie);
 }
--- src/main/java/soighiri/com/coursemoto/service/CategorieServiceImpl.java
+++ src/main/java/soighiri/com/coursemoto/service/CategorieServiceImpl.java
@@ -1,4 +1,47 @@
 package soighiri.com.coursemoto.service;
 
-public class CategorieServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import soighiri.com.coursemoto.dto.CategorieDto;
+import soighiri.com.coursemoto.model.Categorie;
+import soighiri.com.coursemoto.repository.CategorieRepository;
+
+import java.util.List;
+@Service
+public class CategorieServiceImpl implements CategorieService{
+    private CategorieRepository categorieRepository;
+
+    public CategorieServiceImpl(CategorieRepository categorieRepository) {
+        this.categorieRepository = categorieRepository;
+    }
+
+    @Override
+    public Categorie saveCategorieFromCategorieDto(CategorieDto categorieDto) {
+        return null;
+    }
+
+    @Override
+    public Categorie updateCategorieFromCategorieDto(CategorieDto categorieDto) {
+        return null;
+    }
+
+    @Override
+    public void deleteCategorie(Long idCategorie) {
+
+    }
+
+    @Override
+    public Categorie getCategorieById(Long idCategorie) {
+        return null;
+    }
+
+    @Override
+    public List<Categorie> getAllCategories() {
+        return null;
+    }
+
+    @Override
+    public CategorieDto convertEntityToDto(Categorie categorie) {
+        return null;
+    }
 }
