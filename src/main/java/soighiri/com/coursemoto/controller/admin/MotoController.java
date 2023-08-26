3ed19606ef20c051f2b4d7a46f757302aee11776
SaidSoighiri94 <msaidsoighir@gmail.com>
2023-08-26 02:38:19 +0200

    Creation du fichier Moto controller et des vues correspondants

--- src/main/java/soighiri/com/coursemoto/controller/admin/MotoController.java
+++ src/main/java/soighiri/com/coursemoto/controller/admin/MotoController.java
@@ -1,4 +1,27 @@
 package soighiri.com.coursemoto.controller.admin;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import soighiri.com.coursemoto.model.Moto;
+import soighiri.com.coursemoto.service.MotoService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(value = "/admin")
 public class MotoController {
+    private MotoService motoService;
+    @Autowired
+    public MotoController(MotoService motoService) {
+        this.motoService = motoService;
+    }
+    @GetMapping("/moto/listeMotos")
+    public String  index(Model model){
+        List<Moto> motos = motoService.getAllMotos();
+        return "admin/moto/index";
+    }
 }
+
