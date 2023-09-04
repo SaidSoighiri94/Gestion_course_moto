e86763c45e8f16878be113528d0c855f657dc0d3
SaidSoighiri94 <msaidsoighir@gmail.com>
2023-09-04 10:44:36 +0200

    Progrmation du fichier UtilisateurDto

--- src/main/java/soighiri/com/coursemoto/dto/PiloteDto.java
+++ src/main/java/soighiri/com/coursemoto/dto/PiloteDto.java
@@ -34,5 +34,4 @@ public class PiloteDto {
     private String telPilote;
     private Set<Categorie> categories;
 
-
 }
--- src/main/java/soighiri/com/coursemoto/dto/UtilisateurDto.java
+++ src/main/java/soighiri/com/coursemoto/dto/UtilisateurDto.java
@@ -1,4 +1,25 @@
 package soighiri.com.coursemoto.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import soighiri.com.coursemoto.model.Role;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UtilisateurDto {
+    private Long idUtilisateur;
+    @Email(message = "Il faut un email valide")
+    @NotBlank(message = "L'email esr obligatoire")
+    private String email;
+    //TODO Ajouter une annotation perso pour vérifier la concordance entre le mdp1 et mdp2
+    @Size(min = 8)
+    private String mdp1NonEncoder;
+    private String mdp2NonEncoder;
+    private Role role;
+
+
 }
